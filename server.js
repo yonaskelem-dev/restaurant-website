a7f6e4d7d80ed056ea0be87e0eacfcf7faae8c8f
yonnas <yonaskelem789@gmail.com>
2026-05-01 22:24:10 +0300

    fix port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
